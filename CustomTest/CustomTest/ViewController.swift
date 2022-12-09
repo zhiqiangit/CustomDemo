f18fde0b573f3999aa440786b6f4f67a3261987a
黄志强 <hantianit@outlook.com>
2022-12-09 16:32:55 +0800

    test

--- CustomTest/CustomTest/ViewController.swift
+++ CustomTest/CustomTest/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // This is test code
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.addSubview(btn)
     }
 
 
